b5385c122eab418309e1ee587fffe314ff746b1a
asbupsilonno1 <athul.babu.2300@gmail.com>
2023-11-06 09:40:17 +0530

    removed bcrypt

--- helpers/user-helper.js
+++ helpers/user-helper.js
@@ -1,56 +1,50 @@
 var db = require("../config/connection");
 var collection = require("../config/collections");
-const bcrypt = require("bcrypt");
+
 module.exports = {
   doSignup: async (userData) => {
-let alreadyExists= await db.get().collection(collection.USER_COLLECTION).findOne({
-  username:userData.username
-})
-console.log(alreadyExists);
-if(alreadyExists === null){
-  return new Promise(async (resolve, reject) => {
-    userData.password = await bcrypt.hash(userData.password, 10);
-    db.get()
+    let alreadyExists = await db
+      .get()
       .collection(collection.USER_COLLECTION)
-      .insertOne(userData)
-      .then((data) => {
-        console.log(data);
-        resolve(data);
+      .findOne({ username: userData.username });
+    console.log(alreadyExists);
+    if (alreadyExists === null) {
+      return new Promise(async (resolve, reject) => {
+        db.get()
+          .collection(collection.USER_COLLECTION)
+          .insertOne(userData)
+          .then((data) => {
+            console.log(data);
+            resolve(data);
+          });
       });
-  });
-}else{
-  return '$2b$10$S/kte5Zu/jjqn73C3U8XfevCTBteNPtN8RzYGeubL1VQMQgN5J5x.';
-}
-  
+    } else {
+      // If the user already exists, you can return an appropriate response here.
+      // For example, you can return a message or status code.
+      return "User already exists";
+    }
   },
 
-  doLogin:(userData)=>{
-    return new Promise(async(resolve,reject)=>{
-      let loginStatus=false;
-      let response={};
+  doLogin: (userData) => {
+    return new Promise(async (resolve, reject) => {
+      let response = { status: false };
       await console.log(userData.username);
-      // console.log(userData);
-      // console.log("printed the username")
-      let user=await db.get().collection(collection.USER_COLLECTION).findOne({username:userData.username})
-      if(user){
-        bcrypt.compare(userData.password,user.password).then((status)=>{
-      
-          if(status){
-            console.log("login success");
-            response.user=user;
-            response.status=true;
-            resolve(response);
-          }else{
-            console.log("login failed");
-            resolve({status:false});
-          }
-        })
-
-      }else{
-        console.log("user login failed");
-        resolve({status:false});
-
+      let user = await db
+        .get()
+        .collection(collection.USER_COLLECTION)
+        .findOne({ username: userData.username });
+      if (user) {
+        if (userData.password === user.password) {
+          console.log("Login success");
+          response.user = user;
+          response.status = true;
+        } else {
+          console.log("Login failed");
+        }
+      } else {
+        console.log("User not found");
       }
-    })
-  }
+      resolve(response);
+    });
+  },
 };
